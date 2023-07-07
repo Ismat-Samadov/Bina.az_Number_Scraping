317bff6aea5e93bd310d62cd07251bc27f7d53d3
Ismat-Samadov <ismetsemedov@gmail.com>
2023-07-07 14:53:00 +0400

    check

--- scraping_script.py
+++ scraping_script.py
@@ -4,53 +4,11 @@ import asyncio
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 import re
 import aiohttp
-
-async def extract_property_info(url, session):
-    try:
-        async with session.get(url) as response:
-            content = await response.text()
-
-        link_soup = BeautifulSoup(content, 'html.parser')
-
-        phone_number_a = link_soup.find('div', class_='product-phones__list-i').find('a')
-        phone_number = (
-            phone_number_a['href'].replace('tel:', '').replace('-', '').replace(' ', '')
-            if phone_number_a
-            else None
-        )
-
-        owner_name = extract_owner_name(link_soup)
-        owner_category = extract_owner_category(link_soup)
-        category = extract_category(link_soup)
-        floor = extract_floor(link_soup)
-        area = extract_area(link_soup)
-        room_count = extract_room_count(link_soup)
-        description = extract_description(link_soup)
-        mortgage = extract_mortgage(link_soup)
-        price, currency = extract_price(link_soup)
-
-        data = {
-            'url': [url],
-            'phone number': [phone_number],
-            'owner name': [owner_name],
-            'owner category': [owner_category],
-            'category': [category],
-            'floor': [floor],
-            'area': [area],
-            'room count': [room_count],
-            'description': [description],
-            'mortgage': [mortgage],
-            'price': [price],
-            'currency': [currency]
-        }
-        df = pd.DataFrame(data)
-        return df
-
-    except Exception:
-        return None
-
 
 def extract_price(link_soup):
     try:
@@ -59,12 +17,13 @@ def extract_price(link_soup):
             price_val = price_div.find('span', class_='price-val')
             price_cur = price_div.find('span', class_='price-cur')
             if price_val and price_cur:
-                price = re.sub(r'\s+', '', price_val.text.strip())
+                price = re.sub(r'\s+', '', price_val.text.strip())  # Remove whitespace from the price value
                 currency = price_cur.text.strip()
                 return price, currency
     except Exception:
         pass
     return None, None
+
 
 
 def extract_owner_category(link_soup):
@@ -84,6 +43,17 @@ def extract_owner_name(link_soup):
         if owner_name_div:
             owner_name = owner_name_div.text.strip()
             return owner_name
+    except Exception:
+        pass
+    return None
+
+
+def extract_phone_number(link_soup):
+    try:
+        phone_number_div = link_soup.find('div', class_='product-phones__list-i')
+        phone_number_a = phone_number_div.find('a') if phone_number_div else None
+        phone_number = phone_number_a['href'].replace('tel:', '').replace('-', '').replace(' ', '') if phone_number_a else None
+        return phone_number
     except Exception:
         pass
     return None
@@ -156,59 +126,80 @@ def extract_mortgage(link_soup):
         pass
     return None
 
+# Define other extraction functions...
 
-# async def main():
-#     urls = []
+async def extract_property_info(url, session):
+    try:
+        # Chrome settings
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')  # Run Chrome WebDriver in headless mode
+        driver = webdriver.Chrome(options=chrome_options)
 
-#     # Starting link extraction loop
-#     for page in range(1, 3):
-#         print(f"Scraping page {page}...")
-#         url = f'https://bina.az/alqi-satqi?page={page}'
-#         urls.append(url)
+        driver.get(url)
+        time.sleep(2)
 
-#     df_list = []
-#     async with aiohttp.ClientSession() as session:
-#         tasks = []
-#         for url in urls:
-#             task = asyncio.ensure_future(extract_property_info(url, session))
-#             tasks.append(task)
+        # Wait for the phone number element to be present
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'product-phones__btn-value')))
 
-#         property_data = await asyncio.gather(*tasks)
+        # Find the element using XPath
+        element = driver.find_element(By.CLASS_NAME, 'product-phones__btn-value')
+        element.click()   # Click the button
 
-#         for data in property_data:
-#             if data is not None:
-#                 df_list.append(data)
+        # Wait for the updated phone number element to be present
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'product-phones__list-i')))
 
-#     # Concatenate all dataframes in the list
-#     final_df = pd.concat(df_list, ignore_index=True)
+        # Get the updated HTML after clicking the button
+        updated_html = driver.page_source
 
-#     # Process the final_df as desired
-#     # ...
+        # Parse the updated HTML
+        link_soup = BeautifulSoup(updated_html, 'html.parser')
 
-#     print(final_df)
+        # Extract the full phone number
+        phone_number_a = link_soup.find('div', class_='product-phones__list-i').find('a')
+        phone_number = phone_number_a['href'].replace('tel:', '').replace('-', '').replace(' ', '') if phone_number_a else None
 
+        # Extract the owner name
+        owner_name = extract_owner_name(link_soup)
 
-# # Selenium settings
-# chrome_options = Options()
-# chrome_options.add_argument('--headless')  # Run Chrome WebDriver in headless mode
-# driver = webdriver.Chrome(options=chrome_options)
-# driver.quit()  # Close the WebDriver
+        # Extract the owner category
+        owner_category = extract_owner_category(link_soup)
 
-# # Run the main function
-# start_time = time.time()
-# loop = asyncio.get_event_loop()
-# loop.run_until_complete(main())
-# end_time = time.time()
-# execution_time = end_time - start_time
-# print(f"Execution time: {execution_time} seconds")
+        # Extract other information
+        category = extract_category(link_soup)
+        floor = extract_floor(link_soup)
+        area = extract_area(link_soup)
+        room_count = extract_room_count(link_soup)
+        description = extract_description(link_soup)
+        mortgage = extract_mortgage(link_soup)
+        price, currency = extract_price(link_soup)
+
+        driver.quit()  # Close the WebDriver
+
+        # Create a dictionary with the extracted information
+        data = {
+            'url': url,
+            'phone number': phone_number,
+            'owner name': owner_name,
+            'owner category': owner_category,
+            'category': category,
+            'floor': floor,
+            'area': area,
+            'room count': room_count,
+            'description': description,
+            'mortgage': mortgage,
+            'price': price,
+            'currency': currency
+        }
+        return data
+
+    except Exception:
+        return None
+
 async def main():
-    urls = []
-
-    # Starting link extraction loop
-    for page in range(1, 3):
-        print(f"Scraping page {page}...")
-        url = f'https://bina.az/alqi-satqi?page={page}'
-        urls.append(url)
+    urls = [
+        f'https://bina.az/alqi-satqi?page={page}'
+        for page in range(2000, 2319)
+    ]
 
     df_list = []
     async with aiohttp.ClientSession() as session:
@@ -221,19 +212,22 @@ async def main():
 
         for data in property_data:
             if data is not None:
-                df_list.append(data)
-
-    # Check the extracted data
-    for df in df_list:
-        print(df)
+                df = pd.DataFrame([data])
+                df_list.append(df)
 
     # Concatenate all dataframes in the list
     final_df = pd.concat(df_list, ignore_index=True)
 
     # Process the final_df as desired
+    # ...
+
     print(final_df)
 
-# ... (previous code)
+
+# Selenium settings
+chrome_options = Options()
+chrome_options.add_argument('--headless')  # Run Chrome WebDriver in headless mode
+driver = webdriver.Chrome(options=chrome_options)
 
 # Run the main function
 start_time = time.time()
@@ -242,3 +236,6 @@ loop.run_until_complete(main())
 end_time = time.time()
 execution_time = end_time - start_time
 print(f"Execution time: {execution_time} seconds")
+
+# Close the WebDriver
+driver.quit()
